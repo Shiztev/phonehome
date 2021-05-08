6288344fb5cf700e856419925eb803a860167e9f
Stevie Alvarez <stevie.6strings@gmail.com>
2021-05-08 15:25:27 -0400

    stubbed methods, stubbed main and added pseudocode

--- phonehome/HomeServer.java
+++ phonehome/HomeServer.java
@@ -18,4 +18,38 @@ public class HomeServer {
     public HomeServer() {
         proxies = new HashSet<>();
     }
+
+
+    /**
+     * Add a new proxy to the set of proxies.
+     */
+    public void addProxy() {
+
+    }
+
+
+    /**
+     * Remove a proxy when connection is lost.
+     */
+    public void removeProxy() {
+
+    }
+
+
+    /**
+     * Forward a message to all the proxies/clients.
+     */
+    public void sendMsg() {
+
+    }
+
+    
+    public static void main(String[] args) {
+        // Declare server's socket and HomeServer
+
+        // while true
+            // Create a new proxy when connected to (accept and pass socket into proxy)
+            // Pass proxy into a new thread and start it
+            // Add proxy to set of proxies
+    }
 }
