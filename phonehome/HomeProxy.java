20b89ef09640e7d6395d7b4867832219b85f8b7e
Stevie Alvarez <stevie.6strings@gmail.com>
2021-05-08 15:37:02 -0400

    initialized fields, declared necessary server communication fields in newly implemented constructor

--- phonehome/HomeProxy.java
+++ phonehome/HomeProxy.java
@@ -1,14 +1,46 @@
 package phonehome;
 
+import java.net.Socket;
 
 /**
  * PhoneHome Home proxy handler. Handles communication between a {@link PhoneClient}
  * and the {@link HomeServer}. A {@link HomeProxy} is created by the {@link HomeServer}
- *  for each new connection.
+ * for each new connection.
  * 
  * @author Stevie Alvarez
  */
 public class HomeProxy implements Runnable {
+
+    /**
+     * User's Name
+     */
+    private String name;
+
+    /**
+     * Client socket connection.
+     */
+    private Socket phone;
+
+    /**
+     * Parent {@link HomeServer}, which client connected to.
+     */
+    private HomeServer home;
+
+    /**
+     * Create a proxy between a client {@link PhoneClient} connection and 
+     * the {@link HomeServer} home it connected to.
+     * 
+     * @param phone Clients socket connection, accepted from {@link HomeServer}.
+     * @param home This proxies parent {@link HomeServer}.
+     */
+    public HomeProxy(Socket phone, HomeServer home) {
+        this.phone = phone;
+        this.home = home;
+
+        // name should be first message sent
+        // this.name = read();
+    }
+
 
     @Override
     public void run() {
