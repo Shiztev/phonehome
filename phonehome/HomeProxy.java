d6abf0bfcb3a8b9ab8291224f08895ac440fb92e
Stevie Alvarez <stevie.6strings@gmail.com>
2021-05-08 15:18:10 -0400

    init commit

---
+++ phonehome/HomeProxy.java
@@ -0,0 +1,13 @@
+package phonehome;
+
+
+/**
+ * PhoneHome Home proxy handler. Handles communication between a {@link PhoneClient}
+ * and the {@link HomeServer}. A {@link HomeProxy} is created by the {@link HomeServer}
+ *  for each new connection.
+ * 
+ * @author Stevie Alvarez
+ */
+public class HomeProxy {
+    // Must have a hashcode implementation
+}
