b6ea4ce9bb20f5d311439f47fd05cf6c6fce09bf
Stevie Alvarez <stevie.6strings@gmail.com>
2021-05-08 23:35:07 -0400

    extended Comm, initial constructor implementation, stubbed main

--- phonehome/PhoneClient.java
+++ phonehome/PhoneClient.java
@@ -1,11 +1,21 @@
 package phonehome;
 
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Client side of HomePhone. Sends and recieves messages from the Home server.
  * 
  * @author Stevie Alvarez
  */
-public class PhoneClient {
+public class PhoneClient extends Comm {
+
+    public PhoneClient(Socket socket) throws IOException {
+        super(socket);
+    }
     
+    
+    public static void main(String[] args) {
+        
+    }
 }
