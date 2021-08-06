56ca4c3a67d6b3731caf2c21290d0fc3c255093c
8xKetch <stevie.6strings@gmail.com>
2021-08-06 15:42:55 -0400

    moved content to phonehome.sh

--- phonehome/client/client_compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# run client PhoneHome using javafx modules
-javac --module-path ./phonehome/client/assets/javafx-sdk-11.0.2/lib --add-modules javafx.controls ./phonehome/client/PhoneHome.java
