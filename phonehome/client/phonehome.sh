ec8439b86217090dadfafb21863a05ce79692ed8
8xKetch <stevie.6strings@gmail.com>
2021-08-06 15:42:42 -0400

    updated to compile, will update later

--- phonehome/client/phonehome.sh
+++ phonehome/client/phonehome.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# This is the boot script for Phone Home.
+# 
+
+# compile .java files to .class files
+javac --module-path ./phonehome/client/assets/javafx-sdk-11.0.2/lib --add-modules javafx.controls ./phonehome/client/PhoneHome.java
 
 # run client PhoneHome using javafx modules
 java --module-path ./phonehome/client/assets/javafx-sdk-11.0.2/lib --add-modules javafx.controls phonehome.client.PhoneHome
